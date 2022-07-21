bdb4f154bf69386eb02682f75b8241029275fd9c
Apon wuz here <frost_bird@hotmail.com>
2022-07-20 22:47:10 -0500

    trying to line up content on the page

--- scipt.js
+++ scipt.js
@@ -1,4 +1,4 @@
-var apiKey = "&limit=5&appid=aa51d6b8f27c7b03af05a72599465512";
+var apiKey = "&limit=1&appid=aa51d6b8f27c7b03af05a72599465512";
 var url = "http://api.openweathermap.org/geo/1.0/direct?q=";
 
 var userInput = $('.user-input');
