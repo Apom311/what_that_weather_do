fddaba55648807b56e8132220ba98d1ff9054516
Apon wuz here <frost_bird@hotmail.com>
2022-07-20 21:17:14 -0500

    playing with tables and containers

--- scipt.js
+++ scipt.js
@@ -1,2 +1,12 @@
-var apiKey = "&appid=aa51d6b8f27c7b03af05a72599465512";
-var url = "https://api.openweathermap.org/data/2.5/onecall?"
+var apiKey = "&limit=5&appid=aa51d6b8f27c7b03af05a72599465512";
+var url = "http://api.openweathermap.org/geo/1.0/direct?q=";
+
+var userInput = $('.user-input');
+var btn = $('.btn');
+
+
+function searchApi() {
+    fetch(url + userInput + apiKey)
+}
+
+btn.onClick = searchApi();
