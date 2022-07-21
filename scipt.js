27b747361bd9e17e7ca75e2133fe4f0c8e739e9e
Apon wuz here <frost_bird@hotmail.com>
2022-07-21 16:23:04 -0500

    got today's weather working

--- scipt.js
+++ scipt.js
@@ -2,16 +2,16 @@ var cityResult = $("#city");
 var tempResult = $("#temp");
 var humidityResult = $("#humidity");
 var windResult = $("#windSpeed");
-var mainIcon = $("#mainIcon");
 var indexUV = $("#indexUV");
 var forecastCards = $("#forecastCards");
 var forecastRow = $("#forecastRow");
+var cardDisplay = $("#cardDisplay")
 var btnList = $("#btnList");
 var forecastDate = {};
 var forecastIcon = {};
 var forecastTemp = {};
 var forecastHumidity = {};
-var today = moment().format('DD' + '/' + 'MM' + '/' + 'YYYY');
+var today = moment().format('MM' + '/' + 'DD' + '/' + 'YYYY');
 var APIKey = "&units=metric&APPID=aa51d6b8f27c7b03af05a72599465512";
 var url = "https://api.openweathermap.org/data/2.5/weather?q=";
 var citiesArray = JSON.parse(localStorage.getItem("Saved City")) || [];
@@ -72,4 +72,89 @@ function lastSearch () {
         forecast(userInput);
     })
 
+}
+
+function currentWeather(userInput) {
+    var queryURL = url + userInput + APIKey;
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+        var cityInfo = response.name;
+        var country = response.sys.country; 
+        var temp = response.main.temp;
+        var humidity = response.main.humidity;
+        var wind = response.wind.speed;
+        var lat = response.coord.lat;
+        var lon = response.coord.lon;
+        var urlUV = "https://api.openweathermap.org/data/2.5/uvi?" + "lat=" + lat + "&" + "lon=" + lon + "&APPID=aa51d6b8f27c7b03af05a72599465512";
+        cityResult.text(cityInfo + ", " + country + " " + today);
+        tempResult.text("Temperature: " + temp + " ºF");
+        humidityResult.text("Humidity: " + humidity + " %");
+        windResult.text("Wind Speed: " + wind + " MPH");
+        $.ajax({
+            url: urlUV,
+            method: "GET"
+        }).then(function(uvIndex){
+            var UV = uvIndex.value;
+            var colorUV;
+            if (UV <= 3) {
+                colorUV = "green";
+            } else if (UV >= 3 & UV <= 6) {
+                colorUV = "yellow";
+            } else if (UV >= 6 & UV <= 8) {
+                colorUV = "orange";
+            } else {
+                colorUV = "red";
+            }
+            UVIndexText.empty();
+            var UVResultText = $("<p>").attr("class", "card-text").text("UV Index: ");
+            UVResultText.append($("<span>").attr("class", "uvindex").attr("style", ("background-color: " + colorUV)).text(UV))
+            UVIndexText.append(UVResultText);
+            cardDisplay.attr("style", "display: flex; width: 98%");
+        })    
+    })
+}
+
+function forecast (userInput) {
+    forecastRow.empty();
+    forecastCards.empty();
+    var fore5 = $("<h2>").attr("class", "forecast").text("5-Day Forecast: "); 
+    var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + userInput + "&units=metric&APPID=123babda3bc150d180af748af99ad173";
+    $.ajax({
+        url: forecastURL,
+        method: "GET"
+    }).then(function(response){
+        for (var i = 0; i < response.list.length; i += 8){
+            
+            forecastDate[i] = response.list[i].dt_txt;
+            forecastIcon[i] = response.list[i].weather[0].icon;
+            forecastTemp[i] = response.list[i].main.temp; 
+            forecastHum[i] = response.list[i].main.humidity;  
+
+            var newCol2 = $("<div>").attr("class", "col-2");
+            forecastCards.append(newCol2);
+
+            var newDivCard = $("<div>").attr("class", "card text-white bg-primary mb-3");
+            newDivCard.attr("style", "max-width: 18rem;")
+            newCol2.append(newDivCard);
+
+            var newCardBody = $("<div>").attr("class", "card-body");
+            newDivCard.append(newCardBody);
+
+            var newH5 = $("<h5>").attr("class", "card-title").text(moment(forecastDate[i]).format("MMM Do"));
+            newCardBody.append(newH5);
+
+            var newImg = $("<img>").attr("class", "card-img-top").attr("src", "https://openweathermap.org/img/wn/" + forecastIcon[i] + "@2x.png");
+            newCardBody.append(newImg);
+
+            var newPTemp = $("<p>").attr("class", "card-text").text("Temp: " + Math.floor(forecastTemp[i]) + "ºF");
+            newCardBody.append(newPTemp);
+
+            var newPHum = $("<p>").attr("class", "card-text").text("Humidity: " + forecastHum[i] + " %");
+            newCardBody.append(newPHum);
+
+            forecastRow.append(fore5);
+        };
+    })
 }
