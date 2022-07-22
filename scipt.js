cf6bc0f31771f2797c91c5e4c692ff5eadc48f5e
Apon wuz here <frost_bird@hotmail.com>
2022-07-21 22:25:53 -0500

    fixed spelling errors throughout

--- scipt.js
+++ scipt.js
@@ -12,7 +12,7 @@ var forecastIcon = {};
 var forecastTemp = {};
 var forecastHumidity = {};
 var today = moment().format('MM' + '/' + 'DD' + '/' + 'YYYY');
-var APIKey = "&units=metric&APPID=aa51d6b8f27c7b03af05a72599465512";
+var APIKey = "&units=imperial&APPID=aa51d6b8f27c7b03af05a72599465512";
 var url = "https://api.openweathermap.org/data/2.5/weather?q=";
 var citiesArray = JSON.parse(localStorage.getItem("Saved City")) || [];
 var searchInput = $("#searchInput");
@@ -107,10 +107,10 @@ function currentWeather(userInput) {
             } else {
                 colorUV = "red";
             }
-            UVIndexText.empty();
+            indexUV.empty();
             var UVResultText = $("<p>").attr("class", "card-text").text("UV Index: ");
             UVResultText.append($("<span>").attr("class", "uvindex").attr("style", ("background-color: " + colorUV)).text(UV))
-            UVIndexText.append(UVResultText);
+            indexUV.append(UVResultText);
             cardDisplay.attr("style", "display: flex; width: 98%");
         })    
     })
@@ -119,8 +119,8 @@ function currentWeather(userInput) {
 function forecast (userInput) {
     forecastRow.empty();
     forecastCards.empty();
-    var fore5 = $("<h2>").attr("class", "forecast").text("5-Day Forecast: "); 
-    var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + userInput + "&units=metric&APPID=123babda3bc150d180af748af99ad173";
+    var fore5 = $("<h2>").attr("class", "forecast").text("--5-Day Forecast: "); 
+    var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + userInput + "&units=imperial&APPID=123babda3bc150d180af748af99ad173";
     $.ajax({
         url: forecastURL,
         method: "GET"
@@ -130,7 +130,7 @@ function forecast (userInput) {
             forecastDate[i] = response.list[i].dt_txt;
             forecastIcon[i] = response.list[i].weather[0].icon;
             forecastTemp[i] = response.list[i].main.temp; 
-            forecastHum[i] = response.list[i].main.humidity;  
+            forecastHumidity[i] = response.list[i].main.humidity;  
 
             var newCol2 = $("<div>").attr("class", "col-2");
             forecastCards.append(newCol2);
@@ -151,7 +151,7 @@ function forecast (userInput) {
             var newPTemp = $("<p>").attr("class", "card-text").text("Temp: " + Math.floor(forecastTemp[i]) + "ºF");
             newCardBody.append(newPTemp);
 
-            var newPHum = $("<p>").attr("class", "card-text").text("Humidity: " + forecastHum[i] + " %");
+            var newPHum = $("<p>").attr("class", "card-text").text("Humidity: " + forecastHumidity[i] + " %");
             newCardBody.append(newPHum);
 
             forecastRow.append(fore5);
